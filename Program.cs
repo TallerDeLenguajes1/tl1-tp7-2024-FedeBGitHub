77797e4543c8a886dde85e0ca8d4faaa58bc475b
FedeBGitHub <fedebarrionuevo76@gmail.com>
2024-05-27 20:15:38 -0300

    Ejercicio 1 realizado

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using EspacioCalculadora;
+
+int opcion;
+double num;
+Calculadora operacion = new Calculadora();
+
+do
+{
+    Console.WriteLine("######### Calculadora #########");
+    Console.WriteLine("1 - Sumar");
+    Console.WriteLine("2 - Restar");
+    Console.WriteLine("3 - Multiplicar");
+    Console.WriteLine("4 - Dividir");
+    Console.WriteLine("5 - Limpiar");
+    Console.WriteLine("0 - Salir");
+    int.TryParse(Console.ReadLine(),out opcion);
+
+    switch (opcion)
+    {
+        case 1:
+            Console.WriteLine("Sumar | Ingrese el numero: ");
+            double.TryParse(Console.ReadLine(),out num);
+            operacion.Sumar(num);
+        break;
+        case 2:
+            Console.WriteLine("Restar | Ingrese el numero: ");
+            double.TryParse(Console.ReadLine(),out num);
+            operacion.Restar(num);
+        break;
+        case 3:
+            Console.WriteLine("Multiplicar | Ingrese el numero: ");
+            double.TryParse(Console.ReadLine(),out num);
+            operacion.Multiplicar(num);
+        break;
+        case 4:
+            Console.WriteLine("Dividir | Ingrese el numero: ");
+            double.TryParse(Console.ReadLine(),out num);
+            operacion.Dividir(num);
+        break;
+        case 5:
+            operacion.Limpiar();
+        break;
+
+    }
+    Console.WriteLine("El numero resultante es: " + operacion.Resultado);
+} while (opcion!=0);
+
+
+
+
+
+    
